1e90357665ca2a7411cf7828ee0b7261bee7bf9b
Santhosh Kumar <itsmesanthoshkumar69@gmail.com>
2025-05-19 23:41:45 +0530

    Update ForgotPassword.jsx

--- frontend/src/components/ForgotPassword/ForgotPassword.jsx
+++ frontend/src/components/ForgotPassword/ForgotPassword.jsx
@@ -1,47 +1,43 @@
 import { useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import "./ForgotPassword.css";
-
 
 function ForgotPassword() {
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
-    const [showResetButton, setShowResetButton] = useState(false);
+    const [error, setError] = useState("");
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setMessage("");
+        setError("");
+
         try {
             const res = await axios.post("https://material-recommendation-backend.vercel.app/forgot-password", { email });
-            setMessage(res.data.message + ". Token: " + res.data.token);
-            localStorage.setItem("resetToken", res.data.token);
-            setShowResetButton(true);
+            setMessage(res.data.message);  
         } catch (err) {
-            setMessage(err.response?.data?.message || "Something went wrong");
+            setError(err.response?.data?.message || "Something went wrong");
         }
     };
 
     return (
         <div className="container-1">
-            <h2 class="h49">Forgot Password</h2>
+            <h2 className="h49">Forgot Password</h2>
             <form onSubmit={handleSubmit}>
                 <input
-                    class="inputx"
+                    className="inputx"
                     type="email"
                     placeholder="Enter your email"
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     required
                 />
-                <button type="submit">Send Reset Link</button>
+                <button className="btn345" type="submit">Send Reset Link</button>
             </form>
-            <br/><br/>
-            <p>{message}</p> <br/>
-            {showResetButton && (
-                <Link to="/reset-password">
-                    <button>Reset Password</button>
-                </Link>
-            )}
+
+            <br />
+            {message && <p style={{ color: "white" }}>{message}</p>}
+            {error && <p style={{ color: "red" }}>{error}</p>}
         </div>
     );
 }
